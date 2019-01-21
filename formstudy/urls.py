1ac5eff9e0bec18ce06a1a7c6756e4566128042b
frenchkebab <tubowangin@naver.com>
2019-01-21 17:33:06 +0900

    myapp 생성 및 등록

---
+++ formstudy/urls.py
@@ -0,0 +1,8 @@
+# formstudy/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('myapp/', include('myapp.urls')),
+]
